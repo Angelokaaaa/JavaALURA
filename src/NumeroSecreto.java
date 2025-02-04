0eeffdea162eb6a05283d093ac3c2283f5ac7cb8
Angelokaaaa <angela10439@hotmail.com>
2025-02-04 11:58:30 -0300

    programa de numero secreto em java

---
+++ src/NumeroSecreto.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumeroSecreto {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int numeroSecreto = random.nextInt(11);
+        int numero = 0;
+        int tentativas = 5;
+        System.out.println("\nAdivinhe o número secreto!!");
+        System.out.println("Você tem " + tentativas + " tentativas para acertar o número secreto\n");
+
+        while (numero != numeroSecreto) {
+            System.out.println("Digite um número de 1 a 10: ");
+            numero = scanner.nextInt();
+            tentativas --;
+                if (numero > numeroSecreto) {
+                    System.out.println("O número secreto é menor que " + numero);
+                } else if (numero < numeroSecreto) {
+                    System.out.println("O número secreto é maior que " + numero);
+                } else {
+                    break;
+                }
+                if (tentativas == 0 || numero == numeroSecreto) {
+                    break;
+                }
+             System.out.println("Tentativas restantes: " + tentativas);
+        }
+
+        System.out.println("O número secreto era: " + numeroSecreto);
+    }
+}
